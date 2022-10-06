96d6a3737cb7ab924a5788ae57e636a00594b470
dawonseo <oloielo@naver.com>
2022-10-06 17:09:58 +0900

    first commit

---
+++ src/main/java/com/seonko/OurLittleDiary/OurLittleDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.seonko.OurLittleDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurLittleDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurLittleDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seonko/OurLittleDiary/OurLittleDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seonko.OurLittleDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurLittleDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
